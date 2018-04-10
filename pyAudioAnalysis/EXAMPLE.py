381c1610318b67984bad2ce358dc51d41b6126eb
Tyler <tyler.bruno@sjsu.edu>
2018-04-10 16:15:35 -0700

    fixed some more input errors and added data from pyaudioanalaysis git hub

--- pyAudioAnalysis/EXAMPLE.py
+++ pyAudioAnalysis/EXAMPLE.py
@@ -1,7 +1,11 @@
+# from pyAudioAnalysis import audioTrainTest as aT
+#
+# aT.featureAndTrain(["../Emotion Audio/01_Neatral", "../Emotion Audio/05_angry"], 1.0,1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmSMtemp", False)
+# aT.fileClassification("../Emotion Audio/06_Fear/03-01-06-01-01-01-01.wav", "svmSMtemp","svm")
+#
+#
+# Result:(0.0, ([ 0.90156761,  0.09843239]), ['neatural', 'angry'])
+
 from pyAudioAnalysis import audioTrainTest as aT
-
-aT.featureAndTrain(["../Emotion Audio/01_Neatral", "../Emotion Audio/05_angry"], 1.0,1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "model_name", False)
-aT.fileClassification("../Emotion Audio/06_Fear/03-01-06-01-01-01-01.wav", "model_name","svm")
-
-
-# Result:(0.0, array([ 0.90156761,  0.09843239]), ['music', 'speech'])
+aT.featureAndTrain(["classifierData/music","classifierData/speech"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmSMtemp", False)
+aT.fileClassification("data/doremi.wav", "svmSMtemp","svm")
